dedc5617226080bbb18c8a2f252b43bd385b8216
kh45 <khaledhassan45@gmail.com>
2020-07-14 14:10:48 -0400

    list_filtering codewars

--- Leetcode/answer.rb
+++ Leetcode/answer.rb
@@ -1,2 +1,16 @@
-nums1 = [1, 2, 3, 24]
-mamba = [1, 8, 81]
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+
+def two_sum(nums, target)
+    nums.each_with_index do |item, index|
+      answer = 1
+      nums[index + 1, nums.length].each_with_index do |k, index2|
+        if item + k == target
+          return [index, index2 + index + answer]
+        end
+      end
+      answer += 1
+    end    
+end
