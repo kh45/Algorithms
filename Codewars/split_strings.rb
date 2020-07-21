9c22b960caecfe70bba50ee35a03e7e1cd3e0b94
kh45 <khaledhassan45@gmail.com>
2020-07-21 14:13:12 -0400

    split_strings

---
+++ Codewars/split_strings.rb
@@ -0,0 +1,14 @@
+## Description
+
+##Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+##Examples:
+
+# solution('abc') # should return ['ab', 'c_']
+# solution('abcdef') # should return ['ab', 'cd', 'ef']
+
+
+## Best solution
+def solution(str)
+    str.length.even? ? str.scan(/../) : str.scan(/../).push("#{str[-1]}_")
+end
